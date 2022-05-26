63ed9746721ae76998bceb234548a9dfa99344ed
傅子湘 <fuzx@xmpaoyou.com>
2022-05-26 12:26:04 +0800

    feat: 使用D3js绘制打点地图

---
+++ src/components/SvgMap.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="wrapper">
+    <div class="container">
+      <svg class="chart">
+      </svg>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from "vue";
+
+import render from '../mapD3/render'
+
+onMounted(async () => {
+  const res = await fetch('https://geo.datav.aliyun.com/areas_v2/bound/100000_full.json')
+  const geoJson = await res.json()
+  render('.chart', geoJson)
+})
+
+
+</script>
+
+<style scoped>
+.wrapper {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+}
+.container {
+  display: flex;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ src/mapD3/render.js
@@ -0,0 +1,57 @@
+import * as d3 from 'd3'
+
+export default function (className, geoJson) {
+	/**
+	 * 基本配置
+	 */
+	const svgWidth = 961;
+	const svgHeight = 961;
+	const padding = 30;
+
+	const svg = d3.select(className)
+		.attr("height", svgHeight)
+		.attr("width", svgWidth);
+
+	const mapContainer = svg.append("g")
+	.attr("fill", "#444")
+	.attr("cursor", "pointer"); //添加mapContainer装载地图绘制内容
+
+	/**
+	 * 获取投影，并配置
+	 */
+	const x0 = padding;
+	const y0 = padding;
+	const x1 = svgWidth - padding * 2;
+	const y1 = svgHeight - padding * 2;
+	const projection = d3.geoMercator().fitExtent(
+		[
+			[x0, y0], //左上角坐标
+			[x1, y1], //右下角坐标
+		], geoJson);
+
+	/**
+	 * 获取geographic path generator，并配置
+	 */
+	const pathGenerator = d3.geoPath().projection(projection);
+
+	/**
+	 * 利用pathGenerator与features生成path路径，绘制地图
+	 */
+	const mapPath = mapContainer.selectAll("path")
+		.data(geoJson.features) //数据绑定
+		.join("path")
+		.attr("d", pathGenerator) //绘制path
+		.attr("stroke-width", 0.5)
+		.attr("stroke", "#000000")
+		.attr("fill", "#ffffff");
+
+	function zoomed() {
+		const t = d3.event.transform;
+		mapContainer.attr("transform", `translate(${t.x}, ${t.y}) scale(${t.k})`); //改变mapContainer的位置及缩放
+	}
+	const zoom = d3.zoom()
+		.scaleExtent([1, 3])  //设置监听范围
+		.on("zoom", zoomed);  //设置监听事件
+
+	svg.call(zoom); //仍然应用于svg上，但是事件触发时改变的是
+}
