cad3013bf5a4d6d8af8bb6b67a7dd9bd2b3dea60
傅子湘 <fuzx@xmpaoyou.com>
2022-06-08 10:45:23 +0800

    fix: 去除样式覆盖

--- src/leaflet/useMapLayer.js
+++ src/leaflet/useMapLayer.js
@@ -6,11 +6,15 @@ const { TILE_NUM, MaxZoom, MinZoom } = useConfig()
 const mapRef = ref(null)
 
 function renderMap (elem) {
+	const corner1 = L.latLng(0,  0)
+	const corner2 = L.latLng(500, 500)
 	mapRef.value = L
 		.map(elem, {
 			preferCanvas: true,
 			doubleClickZoom: false,
 			crs: L.CRS.Simple, // 简单坐标系
+			attributionControl: false,
+			// maxBounds: L.latLngBounds(corner1, corner2)
 		})
 		.setView([TILE_NUM, TILE_NUM], 4)
 		.setMaxZoom(MaxZoom)
