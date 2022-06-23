33f8547149881e9886eb1267a8014e8224cbe960
傅子湘 <fuzx@xmpaoyou.com>
2022-06-23 18:01:42 +0800

    feat: 更改地图打点计算为中心点

--- src/utils/datafilter.js
+++ src/utils/datafilter.js
@@ -13,34 +13,18 @@ export function pos2polygon(data, options = {}) {
 		direction = 'left-bottom',
 	} = options
 	data.forEach(item => {
-		const w = item.shape ? get(item, _w) : 1
-		const h = item.shape ? get(item, _h) : 1
-		/**
-		 * w 宽度, h:高度, a: x距离左下标距离 b: y距离左下标距离
-		 * a = (b-1)/2
-		 * x: 左横坐标  y: 左纵坐标
-		 * x = x0 +- a
-		 * y = y0 +- b
-		 */
-		const a = (w - 1) / 2
-		const b = (h - 1) / 2
-		// 左下标
-		const x = get(item, _x) - a
-		const y = get(item, _y) - b
-		if (direction === "left-bottom") {
-			item.Polygon = [
-				[
-					[x/1, y/1],
-					[(x + w)/1, y/1],
-					[(x + w)/1, (y + h)/1],
-					[x/1, (y + h)/1],
-					[x/1, y/1],
-				]
+		const offset_x = (get(item, _w) || 1)/2
+		const offset_y = (get(item, _h) || 1)/2
+		const center_x = get(item, _x)
+		const center_y = get(item, _y)
+		item.Polygon = [
+			[
+				[center_x - offset_x, center_y - offset_y],
+				[center_x + offset_x, center_y - offset_y],
+				[center_x + offset_x, center_y + offset_y],
+				[center_x - offset_x, center_y + offset_y],
 			]
-
-		} else if (direction === 'top-left') {
-
-		}
+		]
 	})
 	return data
 }
